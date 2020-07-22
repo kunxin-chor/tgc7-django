c2cde9edfff627afb6bcec971453728078fe764c
kunxin-chor <chorkunxin@yahoo.com>
2020-07-22 12:20:58 +0000

    Added book to author relationship

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 # from the models.py in the same direction as admin.py, import in Book
-from .models import Book, Author
+from .models import Book, Author, Genre, Category, Tag
 
 # Register your models here.
 admin.site.register(Book)
 admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Category)
+admin.site.register(Tag)
