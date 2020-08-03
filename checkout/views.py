0e38def2ee98a4d0759b6a34e4868cc5b3ae5a2e
kunxin-chor <chorkunxin@yahoo.com>
2020-08-03 12:32:33 +0000

    Added how to create Purchases after checkout

--- checkout/views.py
+++ checkout/views.py
@@ -11,6 +11,10 @@ import stripe
 # import in the book
 from books.models import Book
 
+# import in the purchase
+from .models import Purchase
+from django.contrib.auth.models import User
+
 # Create your views here.
 
 
@@ -30,12 +34,14 @@ def checkout(request):
         book_model = get_object_or_404(Book, pk=book_id)
 
         # create line item
+        # you see all the possible properties of a line item at:
+        # https://stripe.com/docs/api/invoices/line_item
         item = {
             "name": book_model.title,
-            # has to be in cents
             "amount": int(book_model.cost * 100),
             "quantity": book['qty'],
-            "currency": "usd"
+            "currency": "usd",
+            "description": book_model.id
         }
 
         line_items.append(item)
@@ -50,6 +56,7 @@ def checkout(request):
     session = stripe.checkout.Session.create(
         payment_method_types=["card"],  # take credit cards
         line_items=line_items,
+        client_reference_id=request.user.id,
         success_url=domain + reverse("checkout_success"),
         cancel_url=domain + reverse("checkout_cancelled")
     )
@@ -106,4 +113,14 @@ def payment_completed(request):
 
 def handle_payment(session):
     print(session)
-    pass
+    user = get_object_or_404(User, pk=session["client_reference_id"])
+
+    for line_item in session["display_items"]:
+        book_id = int(line_item["custom"]["description"])
+        book_model = get_object_or_404(Book, pk=book_id)
+
+        # create the purchase model
+        purchase = Purchase()
+        purchase.book_id = book_model
+        purchase.user_id = user
+        purchase.save()
