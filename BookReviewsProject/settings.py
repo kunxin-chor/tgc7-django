06cf2f5d7f8090230460bb3e3e752b2665dc2ebb
kunxin-chor <chorkunxin@yahoo.com>
2020-08-06 08:16:39 +0000

    updated settings.py to send email

--- BookReviewsProject/settings.py
+++ BookReviewsProject/settings.py
@@ -122,9 +122,19 @@ LOGIN_REDIRECT_URL = '/success'
 
 WSGI_APPLICATION = 'BookReviewsProject.wsgi.application'
 
+TEST_EMAIL = os.environ.get("TEST_EMAIL")
 
 # simulate Django to send out emails
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+if TEST_EMAIL == "1":
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
@@ -189,6 +199,3 @@ CLOUDINARY = {
 
 STRIPE_PUBLISHABLE_KEY = os.environ.get('STRIPE_PUBLISHABLE_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
-
-EMAIL_HOST_PASS = os.environ.get("EMAIL_HOST_PASS")
-EMAIL_HOST_USER =os.environ.get("EMAIL_HOST_USER")
